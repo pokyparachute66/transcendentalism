145fe5e232ad904960a6f46063591ab87c565115
pokyparachute66 <39124886+pokyparachute66@users.noreply.github.com>
2019-04-15 19:34:35 -0500

    main

---
+++ main.js
@@ -0,0 +1,17 @@
+ var mySwiper = new Swiper('.swiper-container', {
+     // Optional parameters
+     direction: 'horizontal',
+     loop: true,
+     watchSlidesProgress: true,
+
+     navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+     }
+ });
+
+
+
+
+
+
